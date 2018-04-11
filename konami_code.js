714da5eb42acfba89c0d573c2e7f55a91dcf8d8a
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-11 20:16:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,19 +5,22 @@ function init() {
   
   
   // Write your JavaScript code inside the init() function
- var index = 0
-	  document.body.addEventListener('keydown', function(events){
-    var key = parseInt(events.detail || events.which);
-   if (key === code[index]){
-     index++;
-    
-     if(index === code.length){
-       
-       alert("Hurray!");
-       index = 0;
-     }
-    
-    } else {
-     index = 0;
-   }
- });}
+
+   let index = 0;
+ 	 
+} 	    document.body.addEventListener('keydown', function(e) {
++      const key = parseInt(e.which || e.detail);
++
++      if (code[index] === key) {
++        index++;
++
++        if (index === code.length) {
++          alert('Hurray!');
++
++          index = 0;
++        }
++      } else {
++        index = 0;
++      }
++    });
++}
